cd7f7210d61ca691c21ed972a77cca95a1f5cc0f
NitoBoritto <184853774+NitoBoritto@users.noreply.github.com>
2026-02-11 21:06:53 +0200

    Fixing streamlit 5

--- pages/📈_Predictor.py
+++ pages/📈_Predictor.py
@@ -11,7 +11,7 @@ root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if root_path not in sys.path:
     sys.path.insert(0, root_path)
 # Import robust extractors for CPU/GPU series
-from custom_transformers import CPUSeriesExtractor, GPUSeriesExtractor
+from custom_transformers import CPUSeriesExtractor, GPUSeriesExtractor, CardinalityReducer
 
 
 # ── Page Config ──
